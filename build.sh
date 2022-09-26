2520e9af5fd2610e78113b32a4c9e6a42e722490
BabySid <blackfox1983@163.com>
2022-09-26 16:20:12 +0800

    update proto

--- build.sh
+++ build.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
-protoc -I. --go_out=. --go_opt=paths=source_relative --go-grpc_out=. --go-grpc_opt=paths=source_relative sodor/sodor.proto
+protoc -I. --go_out=. --go_opt=paths=source_relative sodor/sodor.proto
+protoc -I. --go_out=. --go_opt=paths=source_relative --go-grpc_out=. --go-grpc_opt=paths=source_relative sodor/fat_ctrl.proto
+protoc -I. --go_out=. --go_opt=paths=source_relative --go-grpc_out=. --go-grpc_opt=paths=source_relative sodor/thomas.proto
